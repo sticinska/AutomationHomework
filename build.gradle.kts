b94ce22119223bd52b4c04996eb615b15fb544f4
gita <sticinska@gmail.com>
2026-01-09 12:26:44 +0200

    initial project

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.seleniumhq.selenium:selenium-java:4.39.0")
+    testImplementation("org.testng:testng:7.11.0")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
